6120e09038aff1235d6404df2db741858f7cbb2c
Alex D <147953407+WROG208@users.noreply.github.com>
2024-05-25 22:21:49 -0400

    install.sh
    It will place the script into the folder it needs to be and after changing permissions, it will delete install.sh and convert.py.

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# Installation directory
-INSTALL_DIR="/usr/local/sbin"
-
-# Check if running as root
+# Check if the script is run with root privileges
 if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root" 
-  exit 1
+   echo "This script must be run as root"
+   exit 1
 fi
 
-# Copy the script
-cp convert.py "$INSTALL_DIR/"
-chmod +x "$INSTALL_DIR/convert.py"
+# Copy the convert.py script to /usr/local/bin and make it executable
+cp convert.py /usr/local/bin/convert.py
+chmod +x /usr/local/bin/convert.py
 
-echo "Installation complete. The script has been installed to $INSTALL_DIR/convert.py"
+# Verify if the file was copied successfully
+if [[ -f /usr/local/bin/convert.py ]]; then
+    echo "Installation successful!"
+
+    # Delete the install.sh and convert.py from the current directory
+    rm -f ./install.sh
+    rm -f ./convert.py
+
+    echo "Cleanup completed!"
+else
+    echo "Installation failed. convert.py was not copied to /usr/local/bin."
+fi
