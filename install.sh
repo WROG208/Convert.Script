e7a7d46760e250497683cfe464f35fda783b7bdf
Alex D <147953407+WROG208@users.noreply.github.com>
2024-06-07 14:56:05 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -3,24 +3,73 @@
 # 5/26/2024 By WROG208 \ N4ASS
 # www.lonewolfsystem.org
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
-fi
+SCRIPTS=("menu.sh" "gsmbi.py" "cronJ.sh")
+DESTINATION="/usr/local/sbin"
+
+REPO_DIR=$(basename $(pwd))
+
+move_and_make_executable() {
+  local script=$1
+  if [ ! -f "$script" ]; then
+    echo "Error: $script not found in the current directory."
+    exit 1
+  fi
+
+  sudo mv "$script" "$DESTINATION"
+
+  sudo chmod +x "$DESTINATION/$script"
+}
+
+install_pip() {
+  if ! command -v pip &> /dev/null; then
+    echo "pip is not installed. Installing pip..."
+    wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py
+    sudo python3 get-pip.py
+    rm get-pip.py
+    if ! command -v pip &> /dev/null; then
+      echo "Error: Failed to install pip. Please install it manually."
+      exit 1
+    fi
+  else
+    echo "pip is already installed."
+  fi
+}
+
+install_gtts() {
+  if ! python3 -c "import gtts" &> /dev/null; then
+    echo "gTTS is not installed. Installing now..."
+    sudo pip install gtts
+    if [ $? -ne 0 ]; then
+      echo "Error: Failed to install gTTS. Please install it manually."
+      exit 1
+    fi
+  else
+    echo "gTTS is already installed."
+  fi
+}
 
 
-cp convert.py /usr/local/bin/convert.py
-chmod +x /usr/local/bin/convert.py
+install_pip
 
 
-if [[ -f /usr/local/bin/convert.py ]]; then
-    echo "Installation successful!"
+install_gtts
+
+ 
+for script in "${SCRIPTS[@]}"; do
+  move_and_make_executable "$script"
+done
 
 
-    rm -f ./install.sh
-    rm -f ./convert.py
+for script in "${SCRIPTS[@]}"; do
+  if [ -f "$DESTINATION/$script" ] && [ -x "$DESTINATION/$script" ]; then
+    echo "$script has been installed to $DESTINATION and made executable."
+  else
+    echo "Error: Installation failed for $script."
+    exit 1
+  fi
+done
 
-    echo "Cleanup completed!"
-else
-    echo "Installation failed. convert.py was not copied to /usr/local/bin."
-fi
+cd ..
+rm -rf "$REPO_DIR"
+
+echo "Installation and cleanup completed successfully."
